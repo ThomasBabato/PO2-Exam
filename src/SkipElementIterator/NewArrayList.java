c631ee0c6680033cf0436a887a0ec41488be8a70
ThomasBabato <65108595+ThomasBabato@users.noreply.github.com>
2020-06-21 20:19:16 +0200

    Iterator numeri pari (o dispari)

--- src/SkipElementIterator/NewArrayList.java
+++ src/SkipElementIterator/NewArrayList.java
@@ -1,13 +1,27 @@
 package SkipElementIterator;
 import java.util.ArrayList;
-
+import java.util.Iterator;
 
 
 public class NewArrayList<T> extends ArrayList<T> {
 
     /* override iterator with lambda */
-    public T iterator() {
-        return
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int pos = 0;
+
+            public boolean hasNext() {
+                return pos < size();
+            }
+            /* print only the lements in event position */
+            public T next() {
+                if ( pos%2 == 0 ) {
+                    return get(pos++);
+                }
+                pos++;
+                return null;
+            }
+        };
     }
 
 
--- src/SkipElementIterator/Tester.java
+++ src/SkipElementIterator/Tester.java
@@ -1,9 +1,27 @@
 package SkipElementIterator;
+import java.util.Iterator;
+
+
 
 public class Tester {
     public static void main (String[] args) {
+        NewArrayList<Integer> nal = new NewArrayList<>();
 
+        /* populate */
+        for ( int i = 0; i<10; i++ ) {
+            nal.add(i);
+        }
 
+        /* get Iterator */
+        Iterator i = nal.iterator();
+
+        /* print only even elements */
+        while ( i.hasNext() ) {
+            Object o = i.next();
+            if ( o != null ) {
+                System.out.println(o.toString() + ", ");
+            }
+        }
 
     }
 }
